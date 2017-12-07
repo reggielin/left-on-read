3dc89e063a08da6fbd5acef06cf588bc231b4231
Reggie <regi428@gmail.com>
2017-12-07 08:34:47 -0800

    update A
    still cannot run script.js after dom without console intervention. bubbles continue to stay until you input the if statements in console.

--- A/js/index.js
+++ A/js/index.js
@@ -1,8 +1,18 @@
-var leftonread;
+/*global $ */
+/*jslint browser: true*/
 
-$.getJSON("https://sheetsu.com/apis/v1.0su/2e93602a2b09", function(data) {
-  leftonread = data;
-});
+//$(document).ready(function() {
+//	document.getElementById('You3').style.visibility = "hidden";
+//});
+
+y3Check();
+
+function y3Check(){		
+		if (document.getElementById('Y3').innerHTML.length == 0){
+			document.getElementById('You3').style.visibility = "hidden";
+			}else {document.getElementById('You3').style.visibility = "visible";
+			}
+}
 
 function scrollDown() {
   var focusBottom = document.getElementById("first");
@@ -15,6 +25,7 @@ $("input").keypress(function(event) {
     $('form.chat input[type="submit"]').click();
   }
 });
+
 $('form.chat input[type="submit"]').click(function(event) {
   event.preventDefault();
   var message = $('form.chat input[type="text"]').val();
@@ -37,11 +48,44 @@ $('form.chat input[type="submit"]').click(function(event) {
       $('form.chat > span').removeClass('spinner');
     }, 2000);
   }
-  $('form.chat input[type="text"]').val('');
+
+$('form.chat input[type="text"]').val('');
   scrollDown();
 });
 
-/* DEMO */
-if(parent==top) {
-  $('a.article').show();
-}
+//var leftonread;
+//var i = 0;
+
+//$.ajax({
+//    url: url,
+//    dataType: 'json',
+//    success: function( data )
+//    {
+//        var i = data.i;
+//
+//        $('#You1').text( i.You1 );
+//        $('#Me1').text( i.Me1 );
+//				$('#You2').text( i.You2 );
+//        $('#Me2').text( i.Me2 );
+//				$('#You3').text( i.You3 );
+//        $('#iRead').text( i.iRead );
+//    }
+//});
+
+//$.getJSON("https://sheetsu.com/apis/v1.0su/2e93602a2b09", function(data), success {
+//  leftonread = data;
+//});
+//
+//var you1 = document.getElementById('You1'),
+//    me1 = document.getElementById('Me1'),
+//    you2 = document.getElementById('You2'),
+//		me2 = document.getElementById('Me2'),
+//		you3 = document.getElementById('You3'),
+//		iread = document.getElementById('iRead');
+//
+//you1.innerHTML = leftonread[i].You1;
+//me1.innerHTML = leftonread[i].Me1;
+//you2.innerHTML = leftonread[i].You1;
+//me2.innerHTML = leftonread[i].Me1;
+//you3.innerHTML = leftonread[i].Me;
+//iread.innerHTML = leftonread[i].iRead;
---
+++ A/js/script.js
@@ -0,0 +1,43 @@
+window.delayedMessage = function() {
+	setTimeout(function(){
+	
+		var y1 = document.getElementById('Y1');
+		console.log(y1);
+		var str1 = y1.length;
+		console.log(str1);
+		if (str1 === 0){
+			document.getElementById('You1').style.visibility = "hidden";
+		} else {document.getElementById('You1').style.visibility = "visible";
+		}
+
+		var m1 = document.getElementById('M1').innerHTML;
+		var str2 = m1.length;
+		console.log(str2);
+		if (str2 === 0){
+			document.getElementById('Me1').style.visibility = "hidden";
+		}
+
+		var y2 = document.getElementById('Y2').innerHTML;
+		var str3 = y2.length;
+		console.log(str3);
+		if (str3 === 0){
+			document.getElementById('You2').style.visibility = "hidden";
+		}
+
+		var m2 = document.getElementById('M2').innerHTML;
+		var str4 = m2.length;
+		console.log(str4);
+		if (str4 === 0){
+			document.getElementById('Me2').style.visibility = "hidden";
+		}
+
+		var y3 = document.getElementById('Y3').innerHTML;
+		var str5 = y3.length;
+		console.log(str5);
+		if (str5 === 0){
+			document.getElementById('You3').style.visibility = "hidden";
+		}else {document.getElementById('You3').style.visibility = "visible";
+		}
+
+	}, 500);
+};
